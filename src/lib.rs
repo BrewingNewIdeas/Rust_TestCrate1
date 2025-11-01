5962f5961f6ad557c27e3f38165fd11acb541312
BrewingNewIdeas <tgbrew12@gmail.com>
2025-10-31 22:24:28 -0400

    test1

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,22 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+pub fn test_mult(left:u64,right:u64)->u64{
+    left*right
+}
+
+// use pyo3::prelude::*;
+
+
+// #[pyfunction]
+// fn hello() -> String {
+//     "Hello from Rust!".to_string()
+// }
+
+// #[pymodule]
+// fn pdb_reader(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    
+//     m.add_function(wrap_pyfunction!(hello, m)?)?;
+//     Ok(())
+// }
